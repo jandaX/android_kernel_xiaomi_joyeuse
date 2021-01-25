647fa2a1f58d15d1ad82f84890a2dbbb6749c938
Alessio Balsini <balsini@google.com>
2021-01-25 17:03:31 +0000

    FROMLIST: fuse: Introduce synchronous read and write for passthrough
    All the read and write operations performed on fuse_files which have the
    passthrough feature enabled are forwarded to the associated lower file
    system file via VFS.
    
    Sending the request directly to the lower file system avoids the
    userspace round-trip that, because of possible context switches and
    additional operations might reduce the overall performance, especially
    in those cases where caching doesn't help, for example in reads at
    random offsets.
    
    Verifying if a fuse_file has a lower file system file associated with
    can be done by checking the validity of its passthrough_filp pointer.
    This pointer is not NULL only if passthrough has been successfully
    enabled via the appropriate ioctl().
    When a read/write operation is requested for a FUSE file with
    passthrough enabled, a new equivalent VFS request is generated, which
    instead targets the lower file system file.
    The VFS layer performs additional checks that allow for safer operations
    but may cause the operation to fail if the process accessing the FUSE
    file system does not have access to the lower file system.
    
    This change only implements synchronous requests in passthrough,
    returning an error in the case of asynchronous operations, yet covering
    the majority of the use cases.
    
    Bug: 179164095
    Link: https://lore.kernel.org/lkml/20210125153057.3623715-6-balsini@android.com/
    Signed-off-by: Alessio Balsini <balsini@android.com>
    Change-Id: Ifbe6a247fe7338f87d078fde923f0252eeaeb668
    Signed-off-by: Alessio Balsini <balsini@google.com>
    (cherry picked from commit ea9685a7f9cb16b30e25386386274fdd30627c3a)
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    (cherry picked from commit 3c396afa32d6da3bfcd3f60b22f915a36a8fab94)
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>
    Signed-off-by: Angga <angga@linuxmail.org>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -975,6 +975,9 @@ static ssize_t fuse_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 		ret_val = generic_file_read_iter(iocb, to);
 
 	return ret_val;
+	if (ff->passthrough.filp)
+		return fuse_passthrough_read_iter(iocb, to);
+	return generic_file_read_iter(iocb, to);
 }
 
 static void fuse_write_fill(struct fuse_req *req, struct fuse_file *ff,
@@ -1217,6 +1220,10 @@ static ssize_t fuse_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 	struct inode *inode = mapping->host;
 	ssize_t err;
 	loff_t endbyte = 0;
+	struct fuse_file *ff = file->private_data;
+
+	if (ff->passthrough.filp)
+		return fuse_passthrough_write_iter(iocb, from);
 
 	if (fuse_is_bad(inode))
 		return -EIO;
