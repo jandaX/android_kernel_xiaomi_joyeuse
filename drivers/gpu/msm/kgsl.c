62a503c57dda465355b3393633b9d2b5ca8e2b3a
anulalat <ndra.bodoh@gmail.com>
2021-02-28 17:47:25 +0800

    gpu: msm: fix ../drivers/gpu/msm/kgsl.c:4943:6: warning: unused variable 'cpu' [-Wunused-variable]         int cpu;

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4996,7 +4996,7 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 {
 	int status = -EINVAL;
 	struct resource *res;
-	int cpu;
+//	int cpu;
 
 	status = _register_device(device);
 	if (status)
