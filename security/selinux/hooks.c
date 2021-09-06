9f4089ccac119235b68e7e83e126f3d527ec2bb5
Mark Salyzyn <salyzyn@google.com>
2021-09-06 19:55:13 +0530

    FROMLIST: Add flags option to get xattr method paired to __vfs_getxattr
    Add a flag option to get xattr method that could have a bit flag of
    XATTR_NOSECURITY passed to it.  XATTR_NOSECURITY is generally then
    set in the __vfs_getxattr path when called by security
    infrastructure.
    
    This handles the case of a union filesystem driver that is being
    requested by the security layer to report back the xattr data.
    
    For the use case where access is to be blocked by the security layer.
    
    The path then could be security(dentry) ->
    __vfs_getxattr(dentry...XATTR_NOSECURITY) ->
    handler->get(dentry...XATTR_NOSECURITY) ->
    __vfs_getxattr(lower_dentry...XATTR_NOSECURITY) ->
    lower_handler->get(lower_dentry...XATTR_NOSECURITY)
    which would report back through the chain data and success as
    expected, the logging security layer at the top would have the
    data to determine the access permissions and report back the target
    context that was blocked.
    
    Without the get handler flag, the path on a union filesystem would be
    the errant security(dentry) -> __vfs_getxattr(dentry) ->
    handler->get(dentry) -> vfs_getxattr(lower_dentry) -> nested ->
    security(lower_dentry, log off) -> lower_handler->get(lower_dentry)
    which would report back through the chain no data, and -EACCES.
    
    For selinux for both cases, this would translate to a correctly
    determined blocked access. In the first case with this change a correct avc
    log would be reported, in the second legacy case an incorrect avc log
    would be reported against an uninitialized u:object_r:unlabeled:s0
    context making the logs cosmetically useless for audit2allow.
    
    This patch series is inert and is the wide-spread addition of the
    flags option for xattr functions, and a replacement of __vfs_getxattr
    with __vfs_getxattr(...XATTR_NOSECURITY).
    
    Signed-off-by: Mark Salyzyn <salyzyn@android.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Acked-by: Jan Kara <jack@suse.cz>
    Acked-by: Jeff Layton <jlayton@kernel.org>
    Acked-by: David Sterba <dsterba@suse.com>
    Acked-by: Darrick J. Wong <darrick.wong@oracle.com>
    Acked-by: Mike Marshall <hubcap@omnibond.com>
    Cc: Stephen Smalley <sds@tycho.nsa.gov>
    Cc: linux-kernel@vger.kernel.org
    Cc: kernel-team@android.com
    Cc: linux-security-module@vger.kernel.org
    
    (cherry picked from (rejected from archive because of too many recipients))
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 133515582
    Bug: 136124883
    Bug: 129319403
    Change-Id: Iabbb8771939d5f66667a26bb23ddf4c562c349a1
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Angga <angga@linuxmail.org>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -556,7 +556,8 @@ static int sb_finish_set_opts(struct super_block *sb)
 			goto out;
 		}
 
-		rc = __vfs_getxattr(root, root_inode, XATTR_NAME_SELINUX, NULL, 0);
+		rc = __vfs_getxattr(root, root_inode, XATTR_NAME_SELINUX, NULL,
+				    0, XATTR_NOSECURITY);
 		if (rc < 0 && rc != -ENODATA) {
 			if (rc == -EOPNOTSUPP)
 				pr_warn("SELinux: (dev %s, type "
@@ -1617,10 +1618,12 @@ static int inode_doinit_with_dentry(struct inode *inode, struct dentry *opt_dent
 		len = INITCONTEXTLEN;
 		context = context_onstack;
 		context[len] = '\0';
-		rc = __vfs_getxattr(dentry, inode, XATTR_NAME_SELINUX, context, len);
+		rc = __vfs_getxattr(dentry, inode, XATTR_NAME_SELINUX, context, len,
+							XATTR_NOSECURITY);
 		if (rc == -ERANGE) {
 			/* Need a larger buffer.  Query for the right size. */
-			rc = __vfs_getxattr(dentry, inode, XATTR_NAME_SELINUX, NULL, 0);
+			rc = __vfs_getxattr(dentry, inode, XATTR_NAME_SELINUX, NULL, 0,
+								XATTR_NOSECURITY);
 			if (rc < 0) {
 				dput(dentry);
 				goto out;
@@ -1633,7 +1636,8 @@ static int inode_doinit_with_dentry(struct inode *inode, struct dentry *opt_dent
 				goto out;
 			}
 			context[len] = '\0';
-			rc = __vfs_getxattr(dentry, inode, XATTR_NAME_SELINUX, context, len);
+			rc = __vfs_getxattr(dentry, inode, XATTR_NAME_SELINUX, context, len,
+								XATTR_NOSECURITY);
 		}
 		dput(dentry);
 		if (rc < 0) {
