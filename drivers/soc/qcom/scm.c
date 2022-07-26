0abe8a2e2a819b3981bf67c47f5024e572103c83
Sultan Alsawaf <sultan@kerneltoast.com>
2022-07-26 04:56:30 +0000

    soc: qcom: Open code alignment for clang

--- drivers/soc/qcom/scm.c
+++ drivers/soc/qcom/scm.c
@@ -666,14 +666,15 @@ static void fill_extra_arg(struct scm_desc *desc, struct scm_extra_arg *argbuf)
 
 static int __scm_call2(u32 fn_id, struct scm_desc *desc, bool retry)
 {
-	struct scm_extra_arg extra_arg_buf __aligned(PAGE_SIZE);
+	u8 buf[PAGE_SIZE + sizeof(struct scm_extra_arg)];
+	struct scm_extra_arg *extra_arg_buf = (void *)PAGE_ALIGN((unsigned long)buf);
 	int ret, retry_count = 0;
 	u64 x0;
 
 	if (unlikely(!is_scm_armv8()))
 		return -ENODEV;
 
-	fill_extra_arg(desc, &extra_arg_buf);
+	fill_extra_arg(desc, extra_arg_buf);
 
 	x0 = fn_id | scm_version_mask;
 
