b69e2bd7772f2d5ce21d2725cd38ec2daebe1deb
Alexander Winkowski <dereference23@outlook.com>
2022-10-01 04:01:51 +0000

    Revert "block: do not use interruptible wait anywhere"
    This reverts commit aa6be396714c79c44d5fa6b4ae09c8090fef8727.

--- block/blk-core.c
+++ block/blk-core.c
@@ -816,6 +816,7 @@ EXPORT_SYMBOL(blk_alloc_queue);
 int blk_queue_enter(struct request_queue *q, bool nowait)
 {
 	while (true) {
+		int ret;
 
 		if (percpu_ref_tryget_live(&q->q_usage_counter))
 			return 0;
@@ -832,11 +833,13 @@ int blk_queue_enter(struct request_queue *q, bool nowait)
 		 */
 		smp_rmb();
 
-		wait_event(q->mq_freeze_wq,
-			   !atomic_read(&q->mq_freeze_depth) ||
-			   blk_queue_dying(q));
+		ret = wait_event_interruptible(q->mq_freeze_wq,
+				!atomic_read(&q->mq_freeze_depth) ||
+				blk_queue_dying(q));
 		if (blk_queue_dying(q))
 			return -ENODEV;
+		if (ret)
+			return ret;
 	}
 }
 
